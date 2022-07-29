896ae4f295a1d64afd40e615618b15c7fd8b40ac
truesephah <49324428+truesephah@users.noreply.github.com>
2022-07-29 16:06:10 -0400

    IT RUNS

---
+++ src/main/java/com/quat/angelrings/AngelRings.java
@@ -0,0 +1,23 @@
+package com.quat.angelrings;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AngelRings implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("angelrings");
+
+	public static final Item ANGEL_RING = new Item(new FabricItemSettings().group(ItemGroup.TOOLS));
+	
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Angel Rings coming to you live from Not Scottland, Minnesota");
+		Registry.register(Registry.ITEM, new Identifier("angelrings", "angel_ring"), ANGEL_RING);
+	}
+}
---
+++ src/main/java/com/quat/angelrings/mixin/AngelRingsMixin.java
@@ -0,0 +1,17 @@
+package com.quat.angelrings.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.World;
+
+@Mixin(PlayerEntity.class)
+public abstract class AngelRingsMixin extends LivingEntity {
+    public AngelRingsMixin(EntityType<? extends LivingEntity> entityType, World world) {
+        super(entityType, world);
+        
+    }
+
+}
